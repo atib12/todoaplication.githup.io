6c465149f1e97e8d98de685fce31c6389e9f40fc
atib12 <111041260+atib12@users.noreply.github.com>
2022-10-15 14:37:07 +0500

    todo

---
+++ app.js
@@ -0,0 +1,445 @@
+// var a= "basit";
+// var b = a.slice(0,1);
+// b = b.toUpperCase();
+// var c= a. slice(1);
+// c = c.toLowerCase();
+// console.log(b+c)
+
+
+// var a= "shaikh shaikh basit atif shaikh";
+// var b = "basit";
+// var c = "abdul basit ahmed";
+// for(var i=0;i< a.length;i++){
+// var txt = a.slice(i,i+5,);
+// if(txt == "basit"){
+// alert("basit has found on this index"+i);
+// var firstletter =a.slice(0,i);
+// var middleletter=c;
+// var otherletter=a.slice(i+5);
+// console.log(firstletter+middleletter+otherletter)
+// }
+// }
+// a = a.replace("basit","abdul basit ahmed");
+// console.log(a);
+
+// var num = Math.round(6.4)
+// console.log(num);
+
+// var num = Math.floor(6.7)
+// console.log(num);
+
+// var num = Math.ceil(3.7)
+// console.log(num);
+
+// var randomNum = Math.random();
+// console.log(randomNum);
+
+// var toss = Math.floor(Math.random()*2+1);
+// console.log(toss);
+
+// var a= 3.8754321234
+// var num=a.toFixed(4);
+// console.log(num);
+
+// var a = 30;
+// var b = parseInt(prompt("enter number to be added"));
+// console.log((c = a+b));
+
+// var a = 30;
+// console.log(typeof a);
+// var b = a.toString();
+// console.log(typeof b)
+
+// var a = 1000030000
+// var b= a.toLocaleString();
+// console.log(b);
+
+// var dice = Math.ceil(Math.random()*6);
+// console.log(dice);
+
+// var todayDate = new Date(); String
+// document.write((todayDate.toString));
+
+// var todayDate = new Date (); 
+
+// console.log(todayDate)
+// console.log(todayDate.getDate())
+// console.log(todayDate.getMonth())
+// console.log(todayDate.getFullYear())
+// console.log(todayDate.getDay())
+// console.log(todayDate.getHours())
+// console.log(todayDate.getMinutes())
+// console.log(todayDate.getSeconds())
+// console.log(todayDate.getTime())    
+// // JAVA SCRIPE TIME
+
+// var date = new Date();
+// var mTodate = date.getTime();
+// console.log(mTodate);
+
+// var specDate = new Date ('01-23-2001');
+// var specToDate = specDate.getTime();
+// console.log(specToDate);
+// var num = mTodate-specToDate;
+
+// var age = (num/3.156e+10)
+// console.log(Math.round(age));
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\]
+
+// var todayDate = new Date(); 
+// var a = prompt("Enter your date")
+// var specDate = new Date(a);
+
+// var specDateTime = specDate.getTime();
+// var currentTime = todayDate.getTime();
+// var diff = currentTime - specDateTime
+
+// var finalDate = diff/(1000 * 60 * 60 * 24 * 365)
+// console.log(finalDate);
+
+// 
+// var date = new Date();
+// document.write(date);
+
+// document.write(date.getHours());
+// document.write(date.getMonth());
+// document.write(date.getFullYear());
+
+// var a = new Date()
+// a.setMonth(0)
+// a.setDate(1)
+// a.setFullYear(2022)
+// a.setHours(1)
+// a.setMinutes(30)
+// a.setSeconds(45)
+// console.log(a)
+
+// function abc(){
+//     console.log("function chala")
+// }
+// abc ()
+// abc ()
+
+// function discount(){
+//     var discountAmount = 200
+//     var actualPrize = 500
+//     console.log(actualPrize - discountAmount)
+// }
+// discount()
+
+// function showDiscount(a){
+// console.log(a)
+// }
+// showDiscount("Atifshaikh");
+
+// function abc(amount,discount){
+//     var finalAmount = amount - discount
+// console.log(finalAmount)
+// }
+// abc(1000,200)
+// abc(500,100)
+// abc(700,300)
+
+
+// function abc (firstname,lastname,email){
+// document.write(firstname)
+// document.write(lastname)
+// document.write(email)
+// }
+// var firstname = prompt("enter your first name") 
+// var lastname = prompt("enter your last name")
+// var email = prompt("enter your email")
+// abc(firstname,lastname,email)
+
+// var a = 23 
+// function abc(){
+//     console.log(a)
+// }
+// abc()
+
+
+// var a 
+// function abc(){
+//     a = 10
+// }
+// abc()
+// console.log(a)
+// function calculate(){
+//     var a =100
+//     var b =20 
+//     var c = a - b
+//     return c
+// }
+// var amount = calculate()
+// console.log(amount)
+
+// var todayDate = new Date (); 
+
+// console.log(todayDate)
+// console.log(todayDate.getDate())
+// console.log(todayDate.getMonth())
+// console.log(todayDate.getFullYear())
+// console.log(todayDate.getDay())
+// console.log(todayDate.getHours())
+// console.log(todayDate.getMinutes())
+// console.log(todayDate.getSeconds())
+// console.log(todayDate.getTime())
+
+///////////////////////////////DATE METHODS ASSIGNMENT # 31-34/// ////////////////////
+//  /////////////////////QUESTION NO 1 ////
+
+// var date = new Date();
+// document.write(date);
+
+// //////////////////////////////QUESTION NO 2  ////
+
+// var mTodate = new Date();
+// console.log(mTodate.getMonth("Septembar"));
+// document.write(mTodate);
+
+                                //  QUESTION NO 3   / / // 
+
+//  var date = new Date();
+//  document.write(date);
+                        //       QUESTION NO 4 / // / / / /
+
+// var a = new Date();
+// var a =prompt("Enter days");
+// alert("Enter");
+// document.write(a);
+
+                            //       QUESTION NO 5   // / / / / / / / 
+
+// var mon = prompt("Enter your month")
+// if(mon >= 15 && mon <= 16){
+//     document.write("first fifteen days of the month");
+// }else if(mon >= 16 && mon <= 17){
+//     document.write("last days of the month");
+// }else {
+//     document.write("");
+// }
+
+// /        /    /  / ///////////     QUESTION NO 6
+
+// var date = new Date();
+// document.write(date);
+// document.write("</br>");
+// document.write(date.getMilliseconds("January 1 1970"));
+// document.write(date.getMilliseconds("January 1 1970"));
+// document.write(date);
+
+                            //   / / / / QUSETI0N NO 8
+// var todayDate = new Date(); String
+// document.write((todayDate.toString)); 
+// var date = laterDate();
+//    document.write(date);                         
+
+        //    QUESTION 13 //////////////////////////
+
+// var dob = new Date();
+//     var month_diff = Date.now() - dob.getTime();
+//     var age_dt = new Date(month_diff);    
+//     var year = age_dt.getUTCFullYear();
+//     var age = Math.abs(year - 1994);
+//  prompt("enter your age");   
+//     document.write("Age of the date entered: " + age + " years");
+ 
+//        ///////         QUESTION 14  //////////////////
+
+// var num = Math.round();
+// var customerName = Arsalan ();
+// document.write(date.getMonth());
+// var a =Net Amount Payable (within Due Date) = Number of units * Charges per unit
+// & Gross Amount Payable (after Due Date) = Net Amount + Late Payment Surcharge
+// prompt("enter your unit");
+// document.write("enter your date");
+
+//                 /////       /////  /Function   Assignment # 35-38/////
+// /////// // / / // / / / // / / // QUSETION 1     / / / 
+
+// function curuentDate()   {
+// document.write("curuentDate");
+// }
+// curuentDate();
+
+//  ////// / / // / / / / QUESTION 2      / / // / / / /
+
+// function abc (firstname,lastname,fullname){
+//     document.write(firstname)
+//     document.write(lastname)
+//     document.write(fullname)
+//     }
+//     var firstname = prompt("enter your first name") 
+//     var lastname = prompt("enter your last name")
+//     var email = prompt("enter your fullname")
+//     abc(firstname,lastname,fullname)
+//     document.write("fullname");
+
+  //   / / / / / // / /QUESTION NO 3   / / / / // / / / / / /
+
+// function sum (a,b){
+// var s = (a+b);
+// return sum;
+// }
+// var s = (20,60);
+// console.log("");
+// document.write("sum");
+// prompt("enter your sum");
+
+//  ///////////////QUESTION NO 4
+
+// var a 
+// function abc(){
+//     a = 10
+// }
+// abc()
+// console.log(a)
+// function calculate(){
+//     var a =100
+//     var b =20 
+//     var c = a - b
+//     return c
+// }
+// var amount = calculate()
+// console.log(amount)
+
+
+// var day = prompt("enter a day");
+// switch (day){
+//   case"monday":
+//   alert("")
+
+// }
+
+// var i=0
+// while(i<= 3) {
+//   console.log(i);
+//   i++;
+// console.log(i);
+// }
+
+// function abc() {
+//   console.log("running...");
+// }
+// function closeBox(){
+//   var box = document.getElementById("box");
+//   console.log(box);
+//   box.style.display="none";
+// }
+
+// function addInput(){
+//   var inp = document.getElementById("inp");
+//   console.log(inp.value);
+//   inp.="123ABC";
+// }
+
+
+// function changeSrc(image){
+//     console.log(image); 
+//     image.src="https://i.pinimg.com/originals/eb/e4/a3/ebe4a37984a8745e78555906765df486.jpg"
+// }
+
+// ============   validateForm --------------------;;;;;;;
+// function validateForm() {
+//   var x = document.forms["myForm"]["fname"].value;
+//   if (x == "") {
+//     alert("Name must be filled out");
+//     return false;
+//   }
+// }
+
+// function validateForm(firstname,lastname,email){
+//   document.write(firstname)
+//   document.write(lastname)
+//   document.write(email)
+//   }
+//   var firstname = prompt("enter your first name") 
+//   var lastname = prompt("enter your last name")
+//   var email = prompt("enter your email")
+//   abc(firstname,lastname,email)
+// document.write("");  
+
+
+// function closeBox(){
+//     var box = document.getElementById("box");
+//     console.log(box);
+//     box.style.display="none";
+//   }
+
+// function abc(){
+
+// }
+
+//  ----------------------- SILIDER IMAGE -----------------------?
+
+// var displayImage=document.getElementById("displayImage");
+// var parentImages=document.getElementById("imgsParent");
+// var allImages=parentImages.getElementsByTagName("img");
+
+// indexNumber=0;  
+// function displayImagehome(){
+
+//     displayImage.src=allImages[indexNumber].src;
+// }
+
+// displayImagehome();
+
+
+// function next(){
+// if(indexNumber==allImages.length-1){
+//     indexNumber=0;
+//     displayImage.src=allImages[indexNumber].src;
+//     displayImagehome();
+//  }else{
+// displayImage.src=allImages[indexNumber++].src;
+// displayImagehome();
+// }
+// }
+
+// function prev(){
+//     if(indexNumber==0){
+//       indexNumber=allImages.length-1;
+//       displayImage.src=allImages[indexNumber--].src;
+//       displayImagehome();  
+//     }
+//     else{}
+//     displayImage.src=allImages[indexNumber--].src;
+//     displayImagehome();
+
+
+//  --------------------- BUTTON --------------
+function del(element){
+  element.parentNode.remove()
+  console.log(element)
+
+}
+
+function generateElement(){
+  var p = document.createElement('p');
+  var text = "ABC XYZ"
+  var txtNode = document.createTextNode(text)
+  p.setAttribute('class','pera')
+
+  // var inp = document.getElementById('inp')
+
+  p.appendChild(txtNode)
+
+  var btn = document.createElement('BUTTON')
+  var btnText = "Delete"
+  btnText = document.createTextNode(btnText)
+  btn.appendChild(btnText)
+  btn.setAttribute('onclick' , 'del(this)')
+  p.appendChild(btn)
+
+  console.log(p)
+var parent = document.getElementById('parent')
+parent.appendChild(p)
+}
+
+var btn = document.createElement('BUTTON')
+  var btnText = "edit"
+  btnText = document.createTextNode(btnText)
+  btn.appendChild(btnText)
+  btn.setAttribute('onclick' , 'del(this)')
+  p.appendChild(btn)
